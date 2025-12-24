4473349bf0da080a4bc8b5a44c8ffc92e5e48c17
AndrZva <andrey.zverev21@gmail.com>
2025-12-24 19:28:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_telegram = request.form.get('button_telegram')
+    button_unity = request.form.get('button_unity')
+    button_web = request.form.get('button_web')
+    return render_template('index.html', button_python=button_python, button_telegram=button_telegram,
+                           button_unity=button_unity, button_web=button_web)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
